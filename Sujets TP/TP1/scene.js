916356c2fd210945b63498a4c07a3febae9a67a4
Francis LAM <f.lam@unistra.fr>
2020-01-28 21:00:19 +0100

    Ajout sujets TP0/1/2 et supports TD1/TD2

---
+++ Sujets TP/TP1/scene.js
@@ -0,0 +1,35 @@
+var W = 1000;
+var H = 700;
+
+var container = document.querySelector('#threejsContainer');
+
+var scene, camera;
+
+function init() {        
+        scene = new THREE.Scene();        
+
+        camera = new THREE.PerspectiveCamera(75, W / H, 0.1, 1000);
+        camera.position.set(0, 10, 10);
+        camera.lookAt(scene.position);
+        scene.add(camera);
+        
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(W, H);
+        container.appendChild(renderer.domElement);
+        
+        var sphere = new THREE.Mesh(
+                new THREE.SphereGeometry(1,20,20),
+                new THREE.MeshBasicMaterial( { color: "#FFFFFF" })
+        );
+        
+        scene.add(sphere);
+}
+
+function animate() { //a compléter        
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);       
+}
+
+
+init();
+animate();
