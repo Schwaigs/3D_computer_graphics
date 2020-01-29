e980844f683bec2e9b7bb3cb3b51cf075220bb49
Sarah Schwaiger <sarah.schwaiger@etu.unistra.fr>
2020-01-29 11:30:53 +0100

    TP1 : Ajout d'un cube dans la scène avec une position autre que l'origine

--- Sujets TP/TP1/scene.js
+++ Sujets TP/TP1/scene.js
@@ -21,8 +21,16 @@ function init() {
                 new THREE.SphereGeometry(1,20,20),
                 new THREE.MeshBasicMaterial( { color: "#FFFFFF" })
         );
+
+        var box1 = new THREE.Mesh(
+                new THREE.BoxGeometry(2,2,2),
+                new THREE.MeshBasicMaterial( { color: "#FF8F77" })
+        );
+        box1.position.set(5,5,5); //origine du cube
+        
         
         scene.add(sphere);
+        scene.add(box1);
 }
 
 function animate() { //a compléter        
